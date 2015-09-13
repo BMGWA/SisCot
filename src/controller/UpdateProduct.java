972326509c54a9b1ee1ad681b10ecb4d4eea7366
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2015-09-13 19:09:52 -0300

    Implementing UpdateProject

---
+++ src/controller/UpdateProduct.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ProductDAO;
+import model.Product;
+
+public class UpdateProduct extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UpdateProduct() {
+		super();		
+	}
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String messageUpdateConfirmation = "Iniciada";
+		boolean wasUpdated = sendToProductDAO(request);
+		
+		if(wasUpdated) {
+			messageUpdateConfirmation = "Produto atualizado com sucesso!";
+		}
+		else {
+			messageUpdateConfirmation = "Produto não foi atualizado!";
+		}
+		
+		request.setAttribute("message", messageUpdateConfirmation);
+		
+		RequestDispatcher rd;
+		rd = request.getRequestDispatcher("/ProductResponse.jsp");
+        rd.forward(request,response);
+	}
+	
+	public boolean sendToProductDAO(HttpServletRequest request) {
+		boolean wasUpdated = false;
+		String actualProductName = request.getParameter("actualname");
+		
+		Product product = new Product();
+		
+		product.setProductName(request.getParameter("name"));
+		product.setProductDescription(request.getParameter("description"));
+		
+		ProductDAO productDAO = new ProductDAO();
+		
+		wasUpdated = productDAO.updateProduct(actualProductName, product);
+		
+		return wasUpdated;
+	}
+}
