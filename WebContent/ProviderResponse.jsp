2b84e2f54852609b2e4d35ad1647114ef98e1251
Iago Rodrigues <iago006@hotmail.com>
2015-09-19 18:16:01 -0300

    IncludeProverView add

---
+++ WebContent/ProviderResponse.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+<title>Produto</title>
+
+<!-- CSS  -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
+<link href="css/materialize.css" type="text/css" rel="stylesheet"
+	media="screen,projection" />
+<link href="css/style.css" type="text/css" rel="stylesheet"
+	media="screen,projection" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+
+	<c:import url="header.jsp" />
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="row ">
+	
+		<div class="card-panel teal">
+			<h3 class="row center"><%=request.getAttribute("mensage")%></h1>
+	
+	</div></div>
+
+</body>
+</html>
---
+++ src/controller/IncludeProvider.java
@@ -0,0 +1,96 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Provider;
+import dao.ProviderDAO;
+
+/**
+ * Servlet implementation class IncludeProvider
+ */
+@WebServlet("/IncludeProvider")
+public class IncludeProvider extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**Creating new Provider
+     * @see HttpServlet#HttpServlet()
+     */
+    public IncludeProvider() {
+        super();
+    }
+    
+    /**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+    
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws 
+    	ServletException, IOException {
+    	String messageAddConfirmation = "Iniciada";
+    	
+		//Get atributes of the IncludeProviderView
+    	String providerCnpj = request.getParameter("cnpj");
+    	String providerName = request.getParameter("name");
+    	String providerEmail = request.getParameter("email");
+    	String providerPassword = request.getParameter("password");
+    	int providerDdd =  Integer.parseInt(request.getParameter("ddd"));
+    	int providerPhone = Integer.parseInt(request.getParameter("phone"));
+    	String providerAdress = request.getParameter("adress");
+    	String providerCity = request.getParameter("city");
+    	String providerState = request.getParameter("state");
+    	int providerZip = Integer.parseInt(request.getParameter("zip"));
+    	
+    	System.out.printf(providerCnpj + providerName + providerEmail + providerPassword + providerDdd,
+				 providerPhone + providerAdress + providerCity + providerState + providerZip);
+    	
+		boolean wasAdd = includeProvider(providerCnpj, providerName, providerEmail, providerPassword, providerDdd,
+				 providerPhone, providerAdress, providerCity, providerState, providerZip);
+		
+		if (wasAdd) {
+			messageAddConfirmation = "Fornecedor Cadastrado com sucesso!";
+		}
+		else {
+			messageAddConfirmation = "Fornecedor não pode ser cadastrado!";
+		}
+		
+		//Set the mensage for send to Product Response
+		request.setAttribute("mensage", messageAddConfirmation);
+		
+		//Dispacher the result from the view of confirmation		
+		RequestDispatcher rd;
+		rd = request.getRequestDispatcher("/ProviderResponse.jsp");
+        rd.forward(request,response);
+    }
+    
+    public boolean includeProvider(String providerCnpj, String providerName, String providerEmail, 
+    		String providerPassword, int providerDdd, int providerPhone, String providerAdress, 
+    		String providerCity, String providerState, int providerZip) {
+    	
+    	boolean wasAdd = false;
+    	
+    	Provider provider = new Provider();
+    	
+    	provider.setProviderAdress(providerAdress);
+    	provider.setProviderCity(providerCity);
+    	provider.setProviderCnpj(providerCnpj);
+    	provider.setProviderDdd(providerDdd);
+    	provider.setProviderEmail(providerEmail);
+    	provider.setProviderName(providerName);
+    	provider.setProviderPassword(providerPassword);
+    	provider.setProviderPhone(providerPhone);
+    	provider.setProviderState(providerState);
+    	provider.setProviderZip(providerZip);
+    	
+    	ProviderDAO providerdao = new ProviderDAO();
+    	
+    	wasAdd = providerdao.insertProvider(provider);
+    	
+    	return wasAdd;
+    }
+}
