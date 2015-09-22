b85a094f706233defba5df96d4f4714226007e71
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2015-09-22 19:41:39 -0300

    Implement ConsultProvider controller

--- src/controller/ConsultProvider.java
+++ src/controller/ConsultProvider.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.ProviderDAO;
+import model.Provider;
 
 /**
  * Servlet implementation class ConsultProvider
@@ -24,7 +28,13 @@ public class ConsultProvider extends HttpServlet {
     }
     
     protected void service(HttpServletRequest request, HttpServletResponse response) throws
-	ServletException, IOException {
+		ServletException, IOException {
+    	ArrayList<Provider> providerList = new ArrayList<Provider>();
+    	ProviderDAO providerDao = new ProviderDAO();
+    	
+    	providerList = providerDao.listProviders();
+    	
+    	request.setAttribute("providers", providerList);
     	
     	RequestDispatcher rd;
 		rd = request.getRequestDispatcher("/IncludeProviderView.jsp");
