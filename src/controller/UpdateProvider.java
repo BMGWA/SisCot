be667cb47c22c559c9ea594ed549dc31aeffc806
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2015-09-22 20:07:48 -0300

    Implement UpdateProvider controller

---
+++ src/controller/UpdateProvider.java
@@ -0,0 +1,64 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ProviderDAO;
+import model.Provider;
+
+@WebServlet("/UpdateProvider")
+public class UpdateProvider extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public UpdateProvider() {
+		super();
+	}
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String messageUpdateConfirmation = "Iniciada";
+		boolean wasUpdated = sendToProviderDAO(request);
+		
+		if(wasUpdated) {
+			messageUpdateConfirmation = "Fornecedor atualizado com sucesso!";
+		}
+		else {
+			messageUpdateConfirmation = "Fornecedor não foi atualizado!";
+		}
+		
+		request.setAttribute("mensage", messageUpdateConfirmation);
+		
+		RequestDispatcher rd;
+		rd = request.getRequestDispatcher("/ProviderResponse.jsp");
+        rd.forward(request,response);
+	}
+	
+	public boolean sendToProviderDAO(HttpServletRequest request) {
+		boolean wasUpdated = false;
+		String actualProviderCnpj = (String) request.getParameter("actualCnpj");
+				
+		Provider provider = new Provider();
+		
+		provider.setProviderCnpj(request.getParameter("cnpj"));
+		provider.setProviderName(request.getParameter("name"));
+		provider.setProviderEmail(request.getParameter("email"));
+		provider.setProviderPassword(request.getParameter("password"));
+		//provider.setProviderDdd(request.getParameter("ddd"));
+		//provider.setProviderPhone(request.getParameter("phone"));
+		provider.setProviderAdress(request.getParameter("adress"));
+		provider.setProviderCity(request.getParameter("city"));
+		provider.setProviderState(request.getParameter("state"));
+		//provider.setProviderZip(request.getParameter("zip"));
+		
+		ProviderDAO providerDAO = new ProviderDAO();
+		
+		wasUpdated = providerDAO.updateProvider(actualProviderCnpj, provider);
+		
+		return wasUpdated;
+	}
+}
