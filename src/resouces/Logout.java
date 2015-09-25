44af9b9dbb91e14e7ee9bdd8fdb45335daf2ff6b
TiagoAssuncao <thiagoribeironiquel@hotmail.com>
2015-09-25 18:52:52 -0300

    Creating a Logout servlet

---
+++ src/resouces/Logout.java
@@ -0,0 +1,39 @@
+package resouces;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Logout() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+    	session.invalidate();
+    	
+    	RequestDispatcher rd;
+		rd = request.getRequestDispatcher("/index.jsp");
+        rd.forward(request,response);
+	}
+
+}
