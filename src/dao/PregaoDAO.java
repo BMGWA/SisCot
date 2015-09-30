eff81f96c640c28c1f56c0fc9ef6f6bf586434c2
Iago Rodrigues <iago006@hotmail.com>
2015-09-30 15:46:23 -0300

    PregaoDAO create

---
+++ src/dao/PregaoDAO.java
@@ -0,0 +1,147 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import model.Pregao;
+
+public class PregaoDAO {
+	private Connection connection;
+	
+	public PregaoDAO() {
+			this.connection = new ConnectionFactory().getConnection();
+	}
+	
+	/**
+	 * Include in the data base a new pregao
+	 * @param A new pregao
+	 * @return wasAdd if the pregao was add
+	 */
+	
+	public boolean includePregao(Pregao pregao) {
+		String sql = "insert into Pregao (managerName, pregaoDate) values (?,?)";
+		Date date = new Date(pregao.getPregaoDate().getTimeInMillis());
+		boolean wasAdd = false;
+		
+		try {
+			//Prepare param to execut the Query
+			PreparedStatement statement =  this.connection.prepareStatement(sql);
+			
+			statement.setString(1, pregao.getManagerName());
+			statement.setDate(1, date, pregao.getPregaoDate());
+			statement.execute();
+			
+			//The product was added
+			wasAdd = true;
+			
+			//Close the operators
+			statement.close();
+			connection.close();
+		} catch(SQLException e) {
+			e.printStackTrace();			
+			throw new RuntimeException(e);
+		}
+		
+		return wasAdd;
+	}
+	
+	/**
+	 * Shows all existing products in the database 
+	 * @return
+	 */
+	public ArrayList<Pregao> listPregao() {
+		String sql = "select * from Pregao";
+		ArrayList<Pregao> pregaoList= new ArrayList<Pregao>();
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+		try {
+			PreparedStatement statement = this.connection.prepareStatement(sql);
+			
+			//Returns a result of the query of search
+			ResultSet rs = statement.executeQuery();	
+			
+			//Stores all the products listed in the array
+			while(rs.next()) {
+				Pregao pregao = new Pregao();
+				Calendar calendar = Calendar.getInstance();
+				
+				calendar.setTime(rs.getDate("pregaoDate"));
+				pregao.setManagerName(rs.getString("managerName"));
+				pregao.setPregaoDate(calendar);
+				pregaoList.add(pregao);
+			}
+			
+			//Close the operators
+			statement.close();
+			connection.close();
+		} catch(SQLException e) {	
+			e.printStackTrace();			
+			throw new RuntimeException(e);
+		}
+		
+		return pregaoList;
+	}
+	
+	public boolean deletePregao(int id) {
+		String sql = "delete from Pregao where id = ?";
+		boolean wasDeleted = false;
+		
+		try {
+			PreparedStatement statement = this.connection.prepareStatement(sql);
+			
+			//Set the first atribute of the query
+			statement.setInt(1, id);
+			statement.execute();
+			
+			wasDeleted = true;
+			
+			//Close the operators
+			statement.close();
+			connection.close();
+		} catch(SQLException e) {
+			e.printStackTrace();			
+			throw new RuntimeException(e);
+		}
+		
+		return wasDeleted;
+	}
+	
+	public boolean updateProduct(int idToUpdate, Pregao pregao) {
+		String sql = "update Pregao set managerName=?, pregaoDate=? where id=?";
+		Date date = new Date(pregao.getPregaoDate().getTimeInMillis());
+		boolean wasUpdated = false;
+
+		try {
+			PreparedStatement statement =  this.connection.prepareStatement(sql);
+			
+			//Set the first atribute of the query
+			statement.setString(1, pregao.getManagerName());
+			statement.setDate(2, date, pregao.getPregaoDate());
+			statement.setInt(3, idToUpdate);
+			
+			statement.executeUpdate();
+			
+			wasUpdated = true;
+			
+			//Close the operators
+			statement.close();
+			connection.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return wasUpdated;
+	}
+	
+}
