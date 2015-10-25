6b05d13d12e4f039418e5ca8266e2de635d7ec6b
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2015-10-25 10:55:37 -0200

    Implementing ReportDAO to reports manager

--- src/dao/ReportDAO.java
+++ src/dao/ReportDAO.java
@@ -1,17 +1,97 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import model.Product;
+import model.Quotation;
 
 public class ReportDAO {
 
+	private Connection connection;
+
+	public ReportDAO() {
+		this.connection = new ConnectionFactory().getConnection();
+	}
+
 	public ArrayList<String> listProductsManager(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select productName from Quotation_Product_Provider " + "where quotationID = ?";
+		ArrayList<String> productsManager = new ArrayList<String>();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		try {
+			PreparedStatement statement = this.connection.prepareStatement(sql);
+			
+			statement.setInt(1, id);
+
+			// Returns a result of the query of search
+			ResultSet rs = statement.executeQuery();
+
+			// Stores all the products listed in the array
+			while (rs.next()) {
+
+				String product;
+				product = rs.getString("productName");
+
+				productsManager.add(product);
+			}
+
+			// Close the operators
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		return productsManager;
 	}
 
 	public ArrayList<String> listProvidersManager(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String sql = "select providerName from Quotation_Product_Provider " + "where quotationID = ?";
+		ArrayList<String> providersManager = new ArrayList<String>();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		try {
+			PreparedStatement statement = this.connection.prepareStatement(sql);
+			
+			statement.setInt(1, id);
+
+			// Returns a result of the query of search
+			ResultSet rs = statement.executeQuery();
+
+			// Stores all the products listed in the array
+			while (rs.next()) {
+
+				String product;
+				product = rs.getString("providerName");
+
+				providersManager.add(product);
+			}
+
+			// Close the operators
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		return providersManager;
 	}
 
 	public ArrayList<String> listProductsProvider(int id) {
--- src/model/ReportProvider.java
+++ src/model/ReportProvider.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 
 import dao.ReportDAO;
 
-public class ReportProvider extends Report{
+public class ReportProvider extends Report {
 
 	@Override
 	public ArrayList<ArrayList> showProducts() {
-ReportDAO reportdao = new ReportDAO();
-		
+		ReportDAO reportdao = new ReportDAO();
+
 		ArrayList<String> listProducts = reportdao.listProductsProvider(getQuotation().getId());
 		ArrayList<String> listProviders = reportdao.listProvidersProvider(getQuotation().getId());
-		
+
 		ArrayList<ArrayList> productsForProvider = new ArrayList<>();
 		productsForProvider.add(listProducts);
 		productsForProvider.add(listProviders);
-		
+
 		return productsForProvider;
-		
+
 	}
 
 }
